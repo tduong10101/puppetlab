8630fd592f6da73ba92ba1349b23d1076683709b
root <root@puppet.codespaces.io>
2021-10-04 12:08:36 +0000

    added tomcat template

--- environments/production/modules/tomcat/manifests/config.pp
+++ environments/production/modules/tomcat/manifests/config.pp
@@ -1,6 +1,6 @@
 class tomcat::config inherits tomcat {
     file {$::tomcat::config_path :
-        source  => 'puppet:///modules/tomcat/tomcat.conf',
+        content => template('tomcat/tomcat.conf.erb'),
         mode    => '0644',
         owner   => $::tomcat::user,
         group   => $::tomcat::group,
--- environments/production/modules/tomcat/manifests/params.pp
+++ environments/production/modules/tomcat/manifests/params.pp
@@ -6,5 +6,18 @@ class tomcat::params {
     $packages = [ 'tomcat', 'tomcat-webapps' ]
     $service_name = 'tomcat'
     $service_state = running
-
+    $catalina_pid = '/var/run/tomcat.pid'
+    $shutdown_verbose = 'false'
+    $shutdown_wait = '30'
+    $security_manager = 'false'
+    $catalina_tmpdir = '/var/cache/tomcat/temp'
+    $jasper_home = '/usr/share/tomcat'
+    $catalina_home = '/usr/share/tomcat'
+    $catalina_base = '/usr/share/tomcat'
+    $java_home = '/usr/lib/jvm/jre'
+    $tomcat_cfg_loaded = '1'
+    $xms = "64m"
+    $xmx = "128m"
+    $maxpermsize = "128M"
+    $java_opts = "-Xms${xms} -Xmx${xmx} -XX:MaxPermSize=${maxpermsize} -Djava.security.egd=file:/dev/./urandom"
 }
